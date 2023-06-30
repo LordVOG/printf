72c70e928368491265bc717a22dd5e47a2102922
phosecode <victorolamy@gmail.com>
2023-06-30 16:48:18 +0100

    Fix flag_checker

--- main.h
+++ main.h
@@ -51,19 +51,5 @@ typedef struct _format
 	int (*f)(va_list);
 } format;
 
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
 
 #endif /** MAIN_H **/
