77a971b558d2f5366ea9f4cb68d44a7c87759b05
phosecode <victorolamy@gmail.com>
2023-06-30 11:35:10 +0100

    Fix print_non_printable code which handles %S format

--- print_non_printable.c
+++ print_non_printable.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-int is_lower(char);
-char *string_to_upp(char *);
-
 /**
  * print_non_printable - Print ascii code in hexadecimal
  * for non printable char
@@ -12,42 +9,14 @@ char *string_to_upp(char *);
  */
 int print_non_printable(va_list list)
 {
-        char *print_buff;
-        int size;
+	char *print_buff;
+	int size;
 
-        print_buff = itoa(va_arg(list, unsigned int), 16);
-        print_buff = string_to_upp(print_buff);
+	print_buff = va_arg(list, char *);
+	if (print_buff == NULL)
+		print_buff = "(null)";
+	size = is_printable((print_buff != NULL) ? print_buff : "(null)");
 
-        size = print((print_buff != NULL) ? print_buff : "NULL");
-
-        return (size);
+	return (size);
 }
 
-/**
- * is_lowercase - check if char are in lowercase
- * @c: Character
- *
- * Return: 1 or 0
- */
-int is_lower(char c)
-{
-        return (c >= 'a' && c <= 'z');
-}
-
-/**
- * string_to_upper - change string of char to uppercase
- * @s: String
- *
- * Return: Uppercase string
- */
-char *string_to_upp(char *s)
-{
-        int indx;
-
-        for (indx = 0; s[indx] != '\0'; indx++)
-        {
-                if (is_lower(s[indx]))
-                        s[indx] -= 32;
-        }
-        return (s);
-}
